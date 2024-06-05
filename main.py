80be30db74d41acebfaa590769b24c3491325259
Дрюня <AndrewDem.In@yandex.ru>
2024-06-05 22:57:44 +0300

    изменил 3 страницы (чё-то добавил)

--- main.py
+++ main.py
@@ -5,12 +5,29 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    # Предположим, что у вас есть список сервисов
+    services = [
+        'Сервис 1', 'Сервис 2', 'Сервис 3', 'Сервис 4',
+        'Сервис 5', 'Сервис 6', 'Сервис 7', 'Сервис 8'
+    ]
+    return render_template('index.html', services=services)
 
 
 @app.route('/about')
 def about():
     return render_template('about.html')
+
+
+@app.route('/services')
+def services():
+    return render_template('services.html')
+
+
+@app.route('/residents')
+def residents_page():
+    residents = [f'Резидент {i}' for i in range(1, 33)]
+
+    return render_template('residents.html', residents=residents)
 
 
 if __name__ == '__main__':
